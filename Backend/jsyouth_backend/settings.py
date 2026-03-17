db97ba42b1319a6da90d1c100c39ac67c51976f8
youngjong <dudwhd96@naver.com>
2026-03-17 23:24:24 +0900

    setting.py

--- Backend/jsyouth_backend/settings.py
+++ Backend/jsyouth_backend/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-%a4a(@y)vj9gn7%8fz=h)=t%x6!yx_=6smt)8u&kyx$bshm=!@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['43.200.254.148', 'localhost', '127.0.0.1']
 
 
 # Application definition
